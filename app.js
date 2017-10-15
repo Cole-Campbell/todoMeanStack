ece9dbe6f7140228a524ffda12ea410aecd05804
Cole-Campbell <cole.william.campbell@gmail.com>
2017-10-15 14:38:53 -0400

    Created API controller to communicate with database using Mongoose. Changed dependencies from devDependencies as they are required in production. Included the controller into the main app file.

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 
 app.set('view engine', 'ejs');
 app.use("/assets", express.static(__dirname + '/public'));
+
+//Adds seed data to Database
 setupController(app);
+apiController(app);
 
 mongoose.connect(config.getDbConnectionString());
 
